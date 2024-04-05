d92641743d2b560f3395ce358b157bab5c071fb0
Donnie <alt.mail.16@gmail.com>
2024-04-05 16:51:26 +0100

    Update alias

--- sync_aliases.sh
+++ sync_aliases.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Define the URL to your raw alias file on GitHub
-ALIAS_URL="https://raw.githubusercontent.com/yourusername/linux-aliases/main/my_aliases"
+ALIAS_URL="https://raw.githubusercontent.com/Donnie7/linux-alias/main/my_alias"
 
-# Determine the shell and set the destination file
+# Define start and end comment markers
+START_COMMENT="# >>> start of my_aliases"
+END_COMMENT="# >>> end of my_aliases"
+
+# Define the destination based on the shell
 if [ -n "$ZSH_VERSION" ]; then
     # Assume Zsh
     DEST="$HOME/.zshrc"
@@ -11,27 +15,13 @@ elif [ -n "$BASH_VERSION" ]; then
     # Assume Bash
     DEST="$HOME/.bashrc"
 else
-    # Try to determine the shell from $SHELL variable
-    case $SHELL in
-        */zsh)
-            # Assume Zsh
-            DEST="$HOME/.zshrc"
-            ;;
-        */bash)
-            # Assume Bash
-            DEST="$HOME/.bashrc"
-            ;;
-        *)
-            # Unknown shell, default to Bash
-            DEST="$HOME/.bashrc"
-            echo "Unknown shell, defaulting to .bashrc"
-            ;;
-    esac
+    # Default to Bash
+    DEST="$HOME/.bashrc"
 fi
 
 # Check if the alias comments exist in the file
-if ! grep -q "# >>> start of my_aliases" "$DEST" || ! grep -q "# >>> end of my_aliases" "$DEST"; then
-    echo -e "\n# >>> start of my_aliases\n# >>> end of my_aliases" >> "$DEST"
+if ! grep -q "$START_COMMENT" "$DEST" || ! grep -q "$END_COMMENT" "$DEST"; then
+    echo -e "\n$START_COMMENT\n$END_COMMENT" >> "$DEST"
 fi
 
 # Download new aliases to a temporary file
@@ -39,8 +29,8 @@ TMP_FILE=$(mktemp)
 curl -s $ALIAS_URL -o $TMP_FILE
 
 # Replace old aliases in the configuration file
-sed -i '/# >>> start of my_aliases/,/# >>> end of my_aliases/{//!d}' "$DEST"
-sed -i "/# >>> end of my_aliases/ r $TMP_FILE" "$DEST"
+sed -i "/$START_COMMENT/,/$END_COMMENT/{//!d}" "$DEST"
+sed -i "/$END_COMMENT/ r $TMP_FILE" "$DEST"
 
 # Remove temporary file
 rm $TMP_FILE
