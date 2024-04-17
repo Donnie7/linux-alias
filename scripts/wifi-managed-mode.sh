1642ad46f5be8f7f5b7ffab677276b0a36355a98
Donnie <alt.mail.16@gmail.com>
2024-04-17 20:59:31 +0100

    Update alias

--- scripts/wifi-managed-mode.sh
+++ scripts/wifi-managed-mode.sh
@@ -2,8 +2,34 @@
 
 set +x
 
-sudo ifconfig wlan0 down
-sudo iwconfig wlan0 mode managed
-sudo ifconfig wlan0 up
 sudo systemctl start NetworkManager.service
 sudo systemctl start wpa_supplicant.service
+
+
+
+#!/bin/bash
+
+# Enable debug mode if needed
+set +x
+
+# Get interface choice from the command line argument ($1)
+interface_choice=$1
+
+# Determine which interface to use based on the input variable
+if [ "$interface_choice" -eq 1 ]; then
+    interface="wlan1"
+else
+    interface="wlan0"
+fi
+
+# Stop network services
+sudo systemctl stop NetworkManager.service
+sudo systemctl stop wpa_supplicant.service
+
+# Set the selected interface to monitor mode
+sudo ifconfig $interface down
+sudo iwconfig $interface mode managed
+sudo ifconfig $interface up
+
+echo "Interface $interface is now in managed mode."
+
